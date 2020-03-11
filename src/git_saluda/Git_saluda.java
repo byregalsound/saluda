a2c98485add183e402a1452ef4ead203f46fed90
alumno <alumno@VirtualBox-DWES>
2020-03-11 13:48:37 +0100

    commit numero 1

---
+++ src/git_saluda/Git_saluda.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_saluda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class Git_saluda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre;
+        Scanner sc = new Scanner(System.in);
+        nombre = sc.nextLine();
+        System.out.println("Introduzca nombre: ");
+        nombre = sc.nextLine();
+        System.out.println("Hola "+nombre);
+    }
+    
+}
